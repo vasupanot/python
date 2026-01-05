b72073b337a8ea8b9f29e27e624933f889f78493
vasupanot <panotvasudev@gmail.com>
2026-01-05 16:52:15 +0530

    Add files via upload

---
+++ 3_personal_detail.py
@@ -0,0 +1,23 @@
+"""write a program to create variables to store and display below person detail
+name
+address
+city
+pincode
+age
+weight
+gender"""
+name=input("Enter your name:")
+address=input("Enter your Address:")
+city=input("Enter your city:")
+pincode=input("Enter you pincode:")
+age=input("Enter your age:")
+weight=input("Enter your weight:")
+gender=input("Enter your gender:")
+print("Person Details:")
+print("Name: ",name)
+print("Address: ",address)
+print("city: ",city)
+print("Pincode :",pincode)
+print("Age: ",age)
+print("Weight: ",weight)
+print("Gender: ",gender)
