c095dfe513c8cd0da709cd7602646adddf918ef4
vasupanot <panotvasudev@gmail.com>
2026-01-05 11:34:09 +0530

    Add files via upload

---
+++ 1_list.py
@@ -0,0 +1,15 @@
+Destination = ["Div","Goa","Surat","Dwarka","Katch"]
+
+Value =["Vasudev",99,99.3,True,False,"Panot",78,88,99,00,"Done"]
+
+
+print(Destination + Value)
+print(Destination[0])
+print(Value[0])
+print(Destination[0:5])
+print(Value[0:5])
+
+print(Destination*2)
+print(Value*2)
+
+print(Value[1:6])
