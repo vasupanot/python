acb48006f7d63c079c5f78e7e82066e3d3cb425b
vasupanot <panotvasudev@gmail.com>
2026-01-05 16:47:42 +0530

    Add files via upload

---
+++ 2_string.py
@@ -0,0 +1,12 @@
+name = "swaminarayan gurukul"
+city = " Bhavnagar"
+print(name) 
+print(name[0]) 
+print(name[1]) 
+print(name[2]) 
+print(name[3]) 
+print(name[0:12]) #swaminarqyan
+print(name[13:20]) 
+print(name[13:]) 
+print(name+city)
+print(city*2)
