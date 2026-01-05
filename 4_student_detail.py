72b56db2eb0be8dc8fe9adbf9a68d530292f5c3c
vasupanot <panotvasudev@gmail.com>
2026-01-05 16:56:55 +0530

    Add files via upload

---
+++ 4_student_detail.py
@@ -0,0 +1,14 @@
+"""create a program to accept below detail from user and display it
+ college Name
+ branch
+ semester
+ hobbies"""
+college_name =input("Enter your college name:")
+branch =input("Enter your branch:")
+semester =input("Enter your semester:")
+hobbies =input("Enter your hobbies:")
+print("\nStudent Details:\n")
+print("College Name:",college_name)
+print("Branch:",branch)
+print("Semester:",semester)
+print("Hobbies:",hobbies)
