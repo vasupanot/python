12d72e74cd3a26e52882e19e3efb95eae48c36db
vasupanot <panotvasudev@gmail.com>
2026-01-06 19:44:20 +0530

    Add files via upload

---
+++ 6_variable.py
@@ -0,0 +1,18 @@
+# hello
+
+Name = "Vasudev"
+address ="Rajapara no:-2"
+city ="bhavnagar"
+pincode =364120
+age = 19
+weight = 72
+gender ="male"
+dob ="23/sept/2005"
+
+print("Name is :",Name)
+print("Address is :",address)
+print("Pincode is :",pincode)
+print("Age is :",age)
+print("Weight is :",weight)
+print("Gender is :",gender)
+print("birth date :",dob)
