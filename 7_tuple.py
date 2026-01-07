b9630a4137a05248571f179fa85ea233bb08d1db
Vasudev Panot <panotvasudev@gmail.com>
2026-01-07 11:35:25 +0530

    Add files via upload

---
+++ 7_tuple.py
@@ -0,0 +1,39 @@
+#Hello
+
+indian_states = (
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+)
+
+print("Full Tuple is :",indian_states) # display tuple
+print("\n\nFirst Five states :",indian_states[0:5]) #display first Five states name
+print("\n\nDisplay 2 to 4",indian_states[1:5]) #display 2 to 4 states
+print("\n\nDisplay All start from 7:",indian_states[7:]) #display all items start from 7th index
+
+#del indian_states[2] #try to remove doesn't support in tuple
