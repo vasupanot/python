ccfe1a3c27f5187e2ecb105b774365b8dfbd65c1
Vasudev Panot <panotvasudev@gmail.com>
2026-01-09 20:18:30 +0530

    Add files via upload

---
+++ 14_Relational_operator.py
@@ -0,0 +1,25 @@
+#Hello
+
+#Relational operator in python 
+
+num1 = int(input("Enter value for num1 :"))
+num2 =int(input("Enter value for num2 :"))
+
+result = num1 == num2
+print(f"{result} = {num1} == {num2}")
+
+result = num1 != num2 
+print(f"{result} = {num1} != {num2}")
+
+result = num1 < num2
+print(f"{result} = {num1} < {num2}")
+
+result = num1 > num2 
+print(f"{result} = {num1} > {num2}")
+
+result = num1 <= num2 
+print(f"{result} = {num1} <= {num2}")
+
+result = num1 >= num2 
+print(f"{result} = {num1} >= {num2}")
+print("Good bye")
