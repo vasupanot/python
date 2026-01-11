62913501c13832029e4709df1d7c99cd15717fee
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:26:58 +0530

    Add files via upload

---
+++ 26_set_intersection.py
@@ -0,0 +1,8 @@
+#Hello
+#Create two sets and find their intersection.
+set1={10,20,30,40,50}
+set2={30,40,50,60,70}
+
+print("first set is :",set1)
+print("second set is :",set2)
+print("set of intersection is :",set1.intersection(set2))
