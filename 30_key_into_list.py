14acd736f867ce8f740c1ff1f89d5fff33af7dfc
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:28:19 +0530

    Add files via upload

---
+++ 30_key_into_list.py
@@ -0,0 +1,10 @@
+#Hello
+#From a dictionary, extract only the keys into a list.
+student={'Tushar':89,
+         'Vasu':99,
+        'Vrukshit':22,
+        'Jigesh':27,
+        'Vivek':20
+        }
+
+print(student.keys())
