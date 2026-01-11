534f754b06f6aafdcaa2912e90d672d0fe7ce82c
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:27:17 +0530

    Add files via upload

---
+++ 27_set_difference.py
@@ -0,0 +1,9 @@
+#Hello
+#Create two sets and find their difference.
+
+set1={10,20,30,40,50}
+set2={30,40,50,60,70}
+
+print("first set is :",set1)
+print("second set is :",set2)
+print("set of Difference is :",set1.difference(set2))
