4ca79b6da62317f9bf141e7b1622f429dc109209
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:25:37 +0530

    Add files via upload

---
+++ 22_list_slicing.py
@@ -0,0 +1,8 @@
+#Hello
+#Reverse a list using slicing (no loop).
+
+list=[11,12,13,14,15,16,17,18,19,20]
+print("Original list is :",list)
+
+rev_list=list[::-1]
+print("Reverse list is :",rev_list)
