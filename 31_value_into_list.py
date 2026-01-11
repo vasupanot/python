c0be0fa403897956cb2eca874638112e9354ff04
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:28:39 +0530

    Add files via upload

---
+++ 31_value_into_list.py
@@ -0,0 +1,11 @@
+#hello
+#From a dictionary, extract only the values into a list.
+
+student={'Tushar':89,
+         'Vasu':99,
+        'Vrukshit':22,
+        'Jigesh':27,
+        'Vivek':20
+        }
+
+print(student.values())
