d5a09a18d12276acf643f3a0fcbb6de5c4494fcd
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:27:35 +0530

    Add files via upload

---
+++ 28_list_sorting.py
@@ -0,0 +1,9 @@
+#Hello
+#Given a list of numbers, sort it in ascending and descending order.
+
+list=[1,5,3,6,2,4,8,7,9,10]
+print("List is :",list)
+list.sort()
+print("List in ascending order :",list)
+list.sort(reverse=True)
+print("List in decending order :",list)
