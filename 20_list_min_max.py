cddb048545329ce57cfaa265a8b4833f974bdd65
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:24:55 +0530

    Add files via upload

---
+++ 20_list_min_max.py
@@ -0,0 +1,10 @@
+#Hello
+#Given a list of numbers, find the maximum and minimum values.
+
+list=[1,2,3,4,5,6,7,8,9,10]
+res=min(list)
+res1=max(list)
+
+print("list is :",list)
+print("minimum number is :",res)
+print("maximum number is :",res1)
