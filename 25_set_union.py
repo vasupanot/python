ecd210298a31b5a14482ff58a6e48dbcf77a9781
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:26:40 +0530

    Add files via upload

---
+++ 25_set_union.py
@@ -0,0 +1,9 @@
+#Hello
+#Create two sets and find their union.
+
+set1={10,20,30,40,50}
+set2={30,40,50,60,70}
+
+print("first set is :",set1)
+print("second set is :",set2)
+print("set of union is :",set1.union(set2))
