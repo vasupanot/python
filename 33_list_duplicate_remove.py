33996670792402b21c11907056da94486411c463
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:29:15 +0530

    Add files via upload

---
+++ 33_list_duplicate_remove.py
@@ -0,0 +1,7 @@
+#Hello
+#Create a list of numbers and remove all duplicate values without using loops.
+
+list=[1,2,3,4,5,6,4,3,2,10]
+print(list)
+rem_list=set(list)
+print("value of list is :",rem_list)
