83ef2e2e72c7dccb0f721bbd180082ae104e3b12
Vasudev Panot <panotvasudev@gmail.com>
2026-01-11 23:28:00 +0530

    Add files via upload

---
+++ 29_sum_of_dictionary.py
@@ -0,0 +1,11 @@
+#Hello
+#Create a dictionary of 5 students with their marks and find the total marks.
+
+student={'Tushar':89,
+         'Vasu':99,
+        'Vrukshit':22,
+        'Jigesh':27,
+        'Vivek':20
+        }
+
+print("Sum of dictionary is :",sum(student.values()))
